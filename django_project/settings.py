430e9307c99461474164ce6850ffca333efc3548
joseolinda <joseolindad@yahoo.com.br>
2024-03-22 13:26:30 -0300

    ✨ feat: Change database configuration. Set postgres as default

--- django_project/settings.py
+++ django_project/settings.py
@@ -76,7 +76,15 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
+    },
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
